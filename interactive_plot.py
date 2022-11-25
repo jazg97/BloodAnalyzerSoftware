4d1714b607e2bdc6a62ec075fc58295e5d4aeebd
José Zapana <jose.zapana.g@upch.pe>
2022-11-25 02:09:44 -0500

    Improve user interaction

--- interactive_plot.py
+++ interactive_plot.py
@@ -9,19 +9,24 @@ plt.style.use('ggplot')
 import numpy as np
 from utils import *
 
-#Class wraper for Canvas and Plotting Capabilities
+#Class wrapper for Canvas and Plotting Capabilities
 class MplCanvas(FigureCanvasQTAgg):
     def __init__(self, parent=None, width=12, height=9, dpi=100, axes=1):
         self.fig = Figure(figsize=(width, height), dpi=dpi)
         self.axs = []
         super().__init__(self.fig)
 
-#Class wraper for multi-item combo box
+#Class wrapper for multi-item combo box
 class CheckableComboBox(QtWidgets.QComboBox):
     def __init__(self):
         super(CheckableComboBox, self).__init__()
         self.view().pressed.connect(self.handleItemPressed)
         self.setModel(QtGui.QStandardItemModel(self))
+        self.setEditable(True)
+        self.lineEdit().setReadOnly(True)
+        palette = QtWidgets.qApp.palette()
+        palette.setBrush(QtGui.QPalette.Base, palette.button())
+        self.lineEdit().setPalette(palette)
         self.selected_items = []
 
     def handleItemPressed(self, index):
@@ -32,9 +37,10 @@ class CheckableComboBox(QtWidgets.QComboBox):
         else:
             item.setCheckState(QtCore.Qt.Checked)
             self.selected_items.append(item.text())
-        #print(self.selected_items)
 
-#Class wraper for Dialog test window
+       #print(self.selected_items)
+
+#Class wrapper for Dialog test window
 class Dialog(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -56,14 +62,29 @@ class Dialog(QtWidgets.QMainWindow):
         self.plot_button = QtWidgets.QPushButton("Generate Plot")
         self.canvas = MplCanvas(self, dpi=100)
         self.toolbar = NavigationToolbar(self.canvas, self)
+
+        self.selected_label = QtGui.QStandardItem("----- Select Patient(s) -----")
+        self.selected_label.setBackground(QtGui.QBrush(QtGui.QColor(150,200,120)))
+        self.selected_label.setSelectable(False)
+            
+        self.feature_label = QtGui.QStandardItem("----- Select Feature(s) -----")
+        self.feature_label.setBackground(QtGui.QBrush(QtGui.QColor(80,120,150)))
+        self.feature_label.setSelectable(False)
+        
+        self.id_box.model().setItem(0,0,self.selected_label)
+        self.id_box.lineEdit().setText("----- Select Patient(s) -----")
+        
         for i in range(len(self.unique_ids)):
             self.id_box.addItem('Patient ID %s' % self.unique_ids[i])
-            item = self.id_box.model().item(i, 0)
+            item = self.id_box.model().item(i+1, 0)
             item.setCheckState(QtCore.Qt.Unchecked)
+
         self.feature_box = CheckableComboBox()
+        self.feature_box.model().setItem(0,0,self.feature_label)
+        self.feature_box.lineEdit().setText("----- Select Feature(s) -----")
         for i in range(len(self.features)):
             self.feature_box.addItem('%s'% self.features[i])
-            item = self.feature_box.model().item(i,0)
+            item = self.feature_box.model().item(i+1,0)
             item.setCheckState(QtCore.Qt.Unchecked)
         first_row.addWidget(self.id_box)
         first_row.addWidget(self.feature_box)
@@ -109,8 +130,8 @@ class Dialog(QtWidgets.QMainWindow):
                 limits = [self.dataframe[feature+'_'+limit][datapoints.index].values[0] for limit in ['LowLimit', 'HighLimit']]
                 data.append(datapoints)
 
-            min_value = np.min(np.array(data).flatten())
-            max_value = np.max(np.array(data).flatten())
+            min_value = np.min(np.hstack(data).flatten())
+            max_value = np.max(np.hstack(data).flatten())
 
             print(min_value)
             print(max_value)
